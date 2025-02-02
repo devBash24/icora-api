47d5e86c119bc945acbee6b27c0e2bbb0c849ec8
Bash John <138396242+devBash24@users.noreply.github.com>
2025-02-02 13:57:33 -0400

    feat: -adjusted rate limiter from 1200 to 1000

--- src/middleware/rateLimiter.ts
+++ src/middleware/rateLimiter.ts
@@ -1,20 +1,23 @@
 import rateLimit from 'express-rate-limit';
 
 export const apiLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 1200, // Limit each IP to 100 requests per windowMs
+    windowMs: 60 * 1000, // 1 minute
+    max: 1000 , // Limit each IP to 100 requests per windowMs
     message: {
         success: false,
+
         error: 'Too many requests from this IP, please try again after 15 minutes'
     },
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+
 });
+
 
 // More strict limiter for sensitive routes
 export const strictLimiter = rateLimit({
     windowMs: 60 * 60 * 1000, // 1 hour
-    max: 50, 
+    max: 100, 
     message: {
         success: false,
         error: 'Too many requests from this IP, please try again after an hour'
